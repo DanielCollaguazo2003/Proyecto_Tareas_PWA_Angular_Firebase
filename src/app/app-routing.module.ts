54d13a900f06f91d602b2039894449a92f4e3bed
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2023-12-05 09:43:06 -0500

    Finalizacion de la version movil de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndiceComponent } from './pages/indice/indice.component';
+import { ListaTareasComponent } from './components/lista-tareas/lista-tareas.component';
 
 const routes: Routes = [
   {path: "home", component: IndiceComponent },
+  {path: "lista", component: ListaTareasComponent},
   {path: "**", redirectTo: 'home'}
 ];
 
